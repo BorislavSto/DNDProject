bdbc930ec48d13180b654027fc5a06fbe5ae7956
bbobbii <bobymanti@gmail.com>
2024-05-12 18:11:35 +0200

    Init work
    Wip: dice system, staring off the dice system

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    public int Roll(int diceMax) // could add modifiers here but i think it should happen wherever this is called
+    {
+
+        return 1;
+    }
+}
